98f44ba8149c719736c69a8eebb305cde2569c44
Innocent Oyebode <oyebodeinnocent@gmail.com>
2025-11-20 23:16:46 +0100

    Delete .next/server directory

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/innocentoyebode/Projects/TheCurensi/Dashboard/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
